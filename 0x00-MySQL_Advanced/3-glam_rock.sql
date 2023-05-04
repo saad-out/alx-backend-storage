f968ef6ad75db774e6d7cb66c19374a9f52b8583
saad-out <outsaad03@gmail.com>
2023-05-04 14:54:40 +0100

    ABS & default formed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,2 +1,2 @@
 -- SQL script that lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, IFNULL(split, YEAR(CURDATE())) - formed AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
+SELECT band_name, ABS(IFNULL(split, YEAR(CURDATE())) - IFNULL(formed, 0)) AS lifespan FROM metal_bands WHERE style LIKE '%Glam rock%' ORDER BY lifespan DESC;
