ee407fb95c53df8f3530fd32ccf75cdb3500ff7b
saad-out <outsaad03@gmail.com>
2023-05-08 23:31:00 +0100

    list documents with python

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+This module contains the function list_all which returns all documents
+in a collection
+"""
+from pymongo.collection import Collection
+from typing import List, Dict
+
+
+def list_all(mongo_collection: Collection) -> List[Dict]:
+    """
+    Returns all documents in a collection
+
+    Args:
+        mongo_collection (Collection): The collection to list from
+
+    Returns:
+        list(dict): The list of documents in the collection
+    """
+    if mongo_collection is None:
+        return []
+
+    return [doc for doc in mongo_collection.find()]
