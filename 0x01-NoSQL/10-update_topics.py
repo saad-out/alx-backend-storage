0f2e768879e9010271be5111bcdfb04273712682
saad-out <outsaad03@gmail.com>
2023-05-09 10:43:43 +0100

    udpate with pymongo

---
+++ 0x01-NoSQL/10-update_topics.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+This module contains the function update_topics that changes all topics of a
+school document based on the name
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Changes all topics of a school document based on the name
+
+    Args:
+        mongo_collection: the pymongo collection object
+        name: the school name to update
+        topics: the list of topics approached in the school
+
+    Returns:
+        Nothing
+    """
+    mongo_collection.update_one({"name": name}, {"$set": {"topics": topics}})
