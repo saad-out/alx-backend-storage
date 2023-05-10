b2643ad99f1a8a741386d5c321663ae759fa3fc0
saad-out <outsaad03@gmail.com>
2023-05-10 15:39:03 +0100

    get and type-cast from Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,7 @@
 This module contains a class Cache that stores an instance of the Redis client
 """
 import redis
-from typing import Union
+from typing import Union, Callable, Optional, Any
 import uuid
 
 
@@ -28,3 +28,50 @@ class Cache:
         key: str = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+            self,
+            key: str,
+            fn: Optional[Callable[[bytes], Any]]
+            ) -> Optional[Any]:
+        """
+        Get data from Redis instance
+
+        Args:
+            key (str): key of the Redis instance
+            fn (Optional[Callable[[bytes], Any]]): function to convert data
+
+        Returns:
+            Optional[Any]: data converted by fn
+        """
+        value: Optional[bytes] = self._redis.get(key)
+        if not value:
+            return None
+
+        if fn:
+            return fn(value)
+        return value
+
+    def get_str(self, key: str) -> Optional[str]:
+        """
+        Get data from Redis instance as string
+
+        Args:
+            key (str): key of the Redis instance
+
+        Returns:
+            Optional[str]: data as string
+        """
+        return self.get(key, str)
+
+    def get_int(self, key: str) -> Optional[int]:
+        """
+        Get data from Redis instance as int
+
+        Args:
+            key (str): key of the Redis instance
+
+        Returns:
+            Optional[int]: data as int
+        """
+        return self.get(key, int)
